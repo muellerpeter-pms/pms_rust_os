0cbf9248b961d0815eb309c0b6c004f69d3127e7
Peter Mueller <peter.mueller1984@web.de>
2021-05-06 08:03:01 +0200

    Hallo Welt in qemu

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,11 +1,28 @@
 #![no_std]
 #![no_main]
 
+
+#![feature(asm)]
 use core::panic::PanicInfo;
+
+static HELLO : &[u8] = b"Hallo Welt\n";
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! { 
-    loop {}
+    
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {
+        unsafe {asm!("hlt"); }
+    }
 }
 
 #[panic_handler]
