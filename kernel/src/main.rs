4231b4f2189775ee1490717e296ae2c094128e01
Peter Mueller <peter.mueller1984@web.de>
2021-05-06 11:43:35 +0200

    eigene Testumgebeung

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,6 +1,18 @@
 #![no_std]
 #![no_main]
+
 #![feature(asm)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Starte {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 mod vga_text;
 mod x86_64;
@@ -14,11 +26,12 @@ pub extern "C" fn _start(_boot_info: &BootInfo) -> ! {
     // Bildschirm leeren und Hintergrundfarbe setzen
     vga_text::WRITER.lock().clear();
 
+#[cfg(test)]
+    test_main();
+
     // Ein kurzes Hallo
     println!("Rust OS");
     println!("Version {}", env!("CARGO_PKG_VERSION"));
-
-    panic!("test!");
 
     loop {
         unsafe {
@@ -31,4 +44,14 @@ pub extern "C" fn _start(_boot_info: &BootInfo) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+
+
+#[cfg(test)]
+mod tests {    
+    #[test_case]
+    fn simpler_test() { // ein belangloser Test, um unsere Testumgebung selbst zu testen
+        assert_eq!(1, 1);
+    }
 }
