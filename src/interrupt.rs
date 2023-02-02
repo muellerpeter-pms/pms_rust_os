814cbd8de55abd3021044bf5893ac0e9af501a68
Peter Mueller <mueller.peter@atz-zwickau.de>
2023-02-02 09:08:13 +0100

    timer interrupt no print!('.');

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -93,7 +93,6 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
 
 /// Handler: timer interrupt
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!(".");
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
