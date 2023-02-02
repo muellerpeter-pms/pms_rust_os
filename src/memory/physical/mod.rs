079f4ad044df9dd847d1c30f72b13d168e5247ca
Peter Mueller <mueller.peter@atz-zwickau.de>
2023-02-02 08:54:48 +0100

    tests für physical memory manager

--- src/memory/physical/mod.rs
+++ src/memory/physical/mod.rs
@@ -1,4 +1,7 @@
 //! Die Verwaltung des physischen Speichers.
+//!
+//!
+//!
 
 use super::Locked;
 use bootloader::bootinfo::{MemoryMap, MemoryRegion, MemoryRegionType};
@@ -8,31 +11,31 @@ use x86_64::{
     PhysAddr,
 };
 
-#[cfg(feature = "verbose")]
+#[cfg(all(feature = "verbose", not(test)))]
 use crate::serial_println;
 
 /// The physical allocator, responsible for paged access to
 /// physical memory.
 /// The allocator uses a bitmap of pages and therefore itself reserves
 /// some pages of memory during initialization.
-pub struct PhysicalMemoryManager {
+pub struct PhysicalMemoryManager<'a> {
     /// Enthält die Memory Map aus dem Bootloader
-    memory_map: Option<&'static MemoryMap>,
+    memory_map: Option<&'a MemoryMap>,
     /// Anzahl der einfach ausgegebenen Seiten
     simple_allocated_pages: usize,
 }
 
 lazy_static! {
     /// Statische Instanz der physischen Speicherverwaltung.
-    pub static ref PMM: Locked<PhysicalMemoryManager> = Locked::new(PhysicalMemoryManager {
+    pub static ref PMM: Locked<PhysicalMemoryManager<'static>> = Locked::new(PhysicalMemoryManager {
         memory_map: None,
         simple_allocated_pages: 0,
     });
 }
 
-impl Locked<PhysicalMemoryManager> {
+impl Locked<PhysicalMemoryManager<'_>> {
     /// Funktion, welche alle von der Bootinfo übergebenen Speicherregionen auflistet.
-    #[cfg(feature = "verbose")]
+    #[cfg(all(feature = "verbose", not(test)))]
     fn list_memory_regions(memory_map: &MemoryMap) {
         let regions = memory_map.iter();
         // filter for usable
@@ -60,7 +63,7 @@ impl Locked<PhysicalMemoryManager> {
         let mut lock = self.lock();
         lock.memory_map = Some(memory_map);
 
-        #[cfg(feature = "verbose")]
+        #[cfg(all(feature = "verbose", not(test)))]
         Self::list_memory_regions(memory_map);
     }
 
@@ -88,25 +91,90 @@ impl Locked<PhysicalMemoryManager> {
     }
 
     /// Gibt eine einzelne Seite<4KiB> zurück.
-    fn pop_simple_allocated_page(&self) -> PhysFrame {
+    fn pop_simple_allocated_page(&self) -> Option<PhysFrame> {
         let mut pages = self.get_page_iterator_from_memory_map();
         let mut pmm = self.lock();
-        let page = pages
-            .nth(pmm.simple_allocated_pages)
-            .expect("Out of Memory!");
+        let page = pages.nth(pmm.simple_allocated_pages);
         pmm.simple_allocated_pages += 1;
         page
     }
 }
 
-unsafe impl FrameAllocator<Size4KiB> for Locked<PhysicalMemoryManager> {
+unsafe impl FrameAllocator<Size4KiB> for Locked<PhysicalMemoryManager<'_>> {
     fn allocate_frame(&mut self) -> Option<x86_64::structures::paging::PhysFrame<Size4KiB>> {
-        Some(self.pop_simple_allocated_page())
+        self.pop_simple_allocated_page()
     }
 }
 
-unsafe impl FrameAllocator<Size2MiB> for PhysicalMemoryManager {
+unsafe impl FrameAllocator<Size2MiB> for Locked<PhysicalMemoryManager<'_>> {
     fn allocate_frame(&mut self) -> Option<x86_64::structures::paging::PhysFrame<Size2MiB>> {
         None
     }
+}
+
+/// Tested, ob eine mögliche Seite vom einfachen physischen Allokator
+/// gefunden und zurück gegeben wird.
+#[test_case]
+fn get_page_simple() {
+    use bootloader::bootinfo::FrameRange;
+    // memory map with 1 page
+    let mm = {
+        let mut mm = MemoryMap::new();
+        mm.add_region(MemoryRegion {
+            range: FrameRange {
+                start_frame_number: 2,
+                end_frame_number: 3,
+            },
+            region_type: MemoryRegionType::Usable,
+        });
+        mm
+    };
+
+    let mut pmm = Locked::new(PhysicalMemoryManager {
+        memory_map: Some(&mm),
+        simple_allocated_pages: 0,
+    });
+
+    let _: PhysFrame<Size4KiB> = pmm.allocate_frame().unwrap();
+}
+
+/// Tested, ob der Allokator bei keiner verfügbaren Seite None zurück gobt.
+#[test_case]
+fn get_no_page_simple() {
+    // memory map with 1 page
+    let mm = MemoryMap::new();
+
+    let mut pmm = Locked::new(PhysicalMemoryManager {
+        memory_map: Some(&mm),
+        simple_allocated_pages: 0,
+    });
+
+    assert_eq!(Option::<PhysFrame<Size4KiB>>::None, pmm.allocate_frame());
+}
+
+/// Tested, ob der Allokator eine Seite findet und danach None
+/// zurück gibt. Dazu muss er intern weiter zählen.
+#[test_case]
+fn get_exact_one_page_simple() {
+    use bootloader::bootinfo::FrameRange;
+    // memory map with 1 page
+    let mm = {
+        let mut mm = MemoryMap::new();
+        mm.add_region(MemoryRegion {
+            range: FrameRange {
+                start_frame_number: 2,
+                end_frame_number: 3,
+            },
+            region_type: MemoryRegionType::Usable,
+        });
+        mm
+    };
+
+    let mut pmm = Locked::new(PhysicalMemoryManager {
+        memory_map: Some(&mm),
+        simple_allocated_pages: 0,
+    });
+
+    let _: PhysFrame<Size4KiB> = pmm.allocate_frame().unwrap();
+    assert_eq!(Option::<PhysFrame<Size4KiB>>::None, pmm.allocate_frame());
 }
